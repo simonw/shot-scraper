e59e87c20f1081cfcaa6ec4a8c94db70995580b1
Simon Willison <swillison@gmail.com>
2022-03-12 15:51:05 -0800

    Updated tagline

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def get_long_description():
 
 setup(
     name="shot-scraper",
-    description="Tool for taking automated screenshots",
+    description="Tools for taking automated screenshots of websites",
     long_description=get_long_description(),
     long_description_content_type="text/markdown",
     author="Simon Willison",
