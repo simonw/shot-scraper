059c7a749380c8f81b5564cf17d23041bc353339
Paolo Melchiorre <paolo@20tab.com>
2023-10-06 16:47:07 +0200

    Upgrade Sphinx and fix doc build warnings

--- docs/conf.py
+++ docs/conf.py
@@ -30,7 +30,9 @@ from subprocess import PIPE, Popen
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["myst_parser"]
+extensions = [
+    "myst_parser",
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
@@ -69,7 +71,7 @@ else:
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = "en"
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
