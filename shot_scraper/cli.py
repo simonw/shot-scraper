8490e1b4e68939233be6803cc10e98d97d4658fe
miccou <30512204+miccou@users.noreply.github.com>
2024-11-19 01:16:10 +0000

    add support for --wait-for and --timeout to pdf command

--- shot_scraper/cli.py
+++ shot_scraper/cli.py
@@ -796,6 +796,12 @@ def javascript(
 @click.option(
     "--wait", type=int, help="Wait this many milliseconds before taking the screenshot"
 )
+@click.option("--wait-for", help="Wait until this JS expression returns true")
+@click.option(
+    "--timeout",
+    type=int,
+    help="Wait this many milliseconds before failing",
+)
 @click.option(
     "--media-screen", is_flag=True, help="Use screen rather than print styles"
 )
@@ -840,6 +846,8 @@ def pdf(
     output,
     javascript,
     wait,
+    wait_for,
+    timeout,
     media_screen,
     landscape,
     format_,
@@ -880,6 +888,7 @@ def pdf(
             bypass_csp=bypass_csp,
             auth_username=auth_username,
             auth_password=auth_password,
+            timeout=timeout,
         )
         page = context.new_page()
         if log_console:
@@ -890,6 +899,8 @@ def pdf(
             time.sleep(wait / 1000)
         if javascript:
             _evaluate_js(page, javascript)
+        if wait_for:
+            page.wait_for_function(wait_for)
 
         kwargs = {
             "landscape": landscape,
